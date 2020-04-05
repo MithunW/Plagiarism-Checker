b1f31206ff882dccbc77c68f7c3c013ceba536e9
Prabhanu <prabhanu96@gmail.com>
2020-04-05 22:16:18 +0530

    implement uploading files to mongoDB(backend functions)

--- ui-module/backend/server.js
+++ ui-module/backend/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const crypto = require('crypto');
+const GridFsStorage = require('multer-gridfs-storage');
+const Grid = require('gridfs-stream');
 
 require('dotenv').config();
 
@@ -18,6 +22,39 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
+// Init gfs
+let gfs;
+
+connection.once('open', () => {
+  // Init stream
+  gfs = Grid(connection.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: "mongodb://localhost:27017/testdb",
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.json({ file: req.file });
+  // res.redirect('/');
+});
 // const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
 
