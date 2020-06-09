10314584a8322379e0faccf693faca1f43c8557c
Hemaka Raveen <hemakaraveenhansika@gmail.com>
2020-06-09 12:08:33 +0530

    update web.plagiarism

--- ui-module/backend/routes/web.plagiarism.js
+++ ui-module/backend/routes/web.plagiarism.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-let Result_model = require('../models/result.model');
+let Result_model = require("../models/result.model");
 const admin = require("firebase-admin");
 const serviceAccount = require("../service-account-key.json");
 
@@ -17,48 +17,55 @@ const oldString = "a really Interesting string with some words".split(" ");
 const newString = sw.removeStopwords(oldString);
 
 var results_list = [];
-var tokenize_document=[];
-var process=0;
-var total_process=0;
-var count=0;
-var userId='';
+var tokenize_document = [];
+var url_list = [];
+var process = 0;
+var total_process = 0;
+var count = 0;
+var userId = "";
 
 const verifyToken = (req, res, next) => {
-    const idToken = req.headers.authorization;
-    // console.log(req.headers);
-    admin.auth().verifyIdToken(idToken)
-      .then(function(decodedToken) {
-        let uid = decodedToken.uid;
-        console.log("token verified");
-        next();
-        // console.log(uid);
-        // ...
-      }).catch(function(error) {
-        res.sendStatus(401);
-        console.log(error);
-      });
+  const idToken = req.headers.authorization;
+  // console.log(req.headers);
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(function (decodedToken) {
+      let uid = decodedToken.uid;
+      // console.log("token verified");
+      next();
+      // console.log(uid);
+      // ...
+    })
+    .catch(function (error) {
+      res.sendStatus(401);
+      console.log(error);
+    });
 };
 
-router.route("/text").post(verifyToken,(req, res) => {
+router.route("/text").post(verifyToken, (req, res) => {
   // console.log(req.headers);
   userId = req.body.userId;
   const document = req.body.text;
 
-  process=0;
-  total_process=0;
+  process = 0;
+  total_process = 0;
   results_list = [];
-  tokenize_document=[];
-  count=0;
+  tokenize_document = [];
+  url_list = [];
+  count = 0;
   // console.log(userId, document);
 
   // 01. tokenize the document
   var tokenizer = new natural.RegexpTokenizer({ pattern: /\./ });
   tokenize_document = tokenizer.tokenize(document);
-  console.log("tokenize document");
+  console.log("tokenize document", tokenize_document.length);
   // console.log(tokenize_document);
 
   // 02. remove stopwords and get keywords
   var keyword_document = [];
+  var word_count = 0;
+  var search_query = "";
 
   tokenize_document.forEach(function (sentence, index) {
     var keyword_sentence = "";
@@ -67,146 +74,130 @@ router.route("/text").post(verifyToken,(req, res) => {
 
     newString.forEach(function (keyword, index) {
       keyword_sentence += keyword + " ";
-    });
+      word_count += 1;
+      if (word_count % 32 == 0) {
+        var i=word_count /32;
+        // console.log(search_query, i);
+        runSample(search_query, i).catch(console.error);
 
+
+        search_query = "";
+      } else {
+        search_query += keyword + " ";
+      }
+    });
     keyword_document.push(keyword_sentence + ".");
+    if(keyword_document.length==tokenize_document.length){
+      console.log("After remove keywords in the tokenize document", keyword_document.length);
+      scrap(url_list);
+    }
   });
 
-  console.log("After remove keywords in the tokenize document");
   // console.log(keyword_document);
+
+  // keyword_document.forEach(function (keyword_sentence, index) {
+  //   var word_count=0;
+  //   var search_query="";
+  //   console.log("key word......");
+  //   console.log(keyword_sentence);
+  //   var keyword_list = keyword_sentence.split(" ");
+  //   keyword_list.forEach(function (keyword, index) {
+  //     word_count+=1
+  //     if(word_count%32==0){
+  //       console.log(search_query);
+  //       console.log(" ");
+  //       search_query="";
+  //     }else{
+  //       search_query+=keyword+" ";
+  //     }
+
+  //   });
+  // });
 
   //03. web search API
   // console.log("Result from search API");
 
-  // async function runSample() {
-  //   const customSearch = google.customsearch("v1");
-  //   const response = await customSearch.cse.list({
-  //     auth: "AIzaSyDft4KwXd4xfoLk6eCrIPPgpsEJXDk57FU",
-  //     cx: "002062490322378734881:fqfodg8bief",
-  //     q: "what is java",
-  //     num: 2,
-  //   });
-  //   // console.log(response.data.items);
-  //   if (response.data.items.length > 0) {
-  //     // console.log(response.data);
-  //     let results = response.data.items;
+  async function runSample(search_query, i) {
+    const customSearch = google.customsearch("v1");
+    const response = await customSearch.cse.list({
+      auth: "AIzaSyDft4KwXd4xfoLk6eCrIPPgpsEJXDk57FU",
+      cx: "002062490322378734881:fqfodg8bief",
+      q: search_query,
+      num: 1,
+    });
+    // console.log(response.data.items);
+    // if (response.data.items.length > 0) {
+      // console.log(response);
+      let results = response;
+      console.log(i);
+      console.log(search_query);
+      console.log(results.data.items[0].link);
+      console.log(" ------------------------------------- ");
+      url_list.push(results.data.items[0].link);
 
-  //     results.forEach(function (result) {
-  //       console.log("*****************************");
-  //       console.log(result);
-  //     });
-  //   }
-  // }
-  // runSample().catch(console.error);
+      // let results = response.data.items;
 
-  // tokenize_document.forEach(function(sentence, index) {
-  //   async function runSample() {
-  //     const customSearch = google.customsearch('v1');
-  //     const response= await customSearch.cse.list({
-  //       auth:'AIzaSyDft4KwXd4xfoLk6eCrIPPgpsEJXDk57FU',
-  //       cx:'002062490322378734881:fqfodg8bief',
-  //       q:sentence,
-  //       num:2
-  //     });
-  //     // console.log(response.data.items);
-  //     if(response.data.items.length>0){
-  //       // console.log(response.data);
-  //       let results=response.data.items;
-
-  //       results.forEach(function(result) {
-  //         console.log("*****************************");
-  //         console.log(result, index);
-  //       });
-  //     }
-  //   }
-  //   runSample().catch(console.error);
-  // });
-
-  //04.string matching
-  // var similarity = stringSimilarity.compareTwoStrings("hea", "healed");
-  // console.log("similarity", similarity * 100);
-  // var matches = stringSimilarity.findBestMatch("healed", [
-  //   "edward",
-  //   "sealed",
-  //   "theatre",
-  // ]);
-  // console.log("matches", matches);
+      // results.forEach(function (result) {
+      //   console.log("*****************************");
+      //   console.log(result);
+      // });
+    // }
+  }
 
   //05. wordnet
-  let lemma = [];
-  wordnet.lookup("see", function (results) {
-    results.forEach(function (result) {
-      lemma.push(result.lemma.replace("_", " "));
-    });
+  // let lemma = [];
+  // wordnet.lookup("see", function (results) {
+  //   results.forEach(function (result) {
+  //     lemma.push(result.lemma.replace("_", " "));
+  //   });
 
-    var synonyms = new Set(lemma);
-    console.log([...synonyms]);
-  });
-
-  // google search api
-  //   async function runSample() {
-  //     const customSearch = google.customsearch('v1');
-  //     const response= await customSearch.cse.list({
-  //       auth:'AIzaSyDft4KwXd4xfoLk6eCrIPPgpsEJXDk57FU',
-  //       cx:'002062490322378734881:fqfodg8bief',
-  //       q:'what is java',
-  //       num:2
-  //     });
-  //     // console.log(response.data.items);
-  //     if(response.data.items.length>0){
-  //       // console.log(response.data);
-  //       let results=response.data.items;
-
-  //       results.forEach(function(result) {
-  //         console.log("*****************************");
-  //         console.log(result);
-  //       });
-  //     }
-  //   }
-  //   runSample().catch(console.error);
-  // console.log("***********************************************************");
+  //   var synonyms = new Set(lemma);
+  //   console.log([...synonyms]);
+  // });
 
   //06. extract page content - web scraping
-  const url_list = ["https://en.wikipedia.org/wiki/Java_(programming_language)","https://en.wikipedia.org/wiki/Science","https://www.space.com/56-our-solar-system-facts-formation-and-discovery.html"];
-  const target_list = [];
+  // var url_list = [
+  //   "https://en.wikipedia.org/wiki/Java_(programming_language)",
+  //   "https://en.wikipedia.org/wiki/Science",
+  //   "https://www.space.com/56-our-solar-system-facts-formation-and-discovery.html",
+  // ];
 
-  url_list.forEach(function (url, index) {
-    puppeteer
-    .launch()
-    .then(function (browser) {
-      return browser.newPage();
-    })
-    .then(function (page) {
-      return page.goto(url).then(function () {
-        return page.content();
-      });
-    })
-    .then(function (html) {
-      // console.log(html);
-      data = extractor(html);
-      console.log("web scraping");
-      // console.log(data.text);
-      // target_text=data;
-      // compareText(data.text);
-      target_list.push(data.text);
+  function scrap(url_list) {
+    const target_list = [];
+    url_list.forEach(function (url, index) {
+      puppeteer
+        .launch()
+        .then(function (browser) {
+          return browser.newPage();
+        })
+        .then(function (page) {
+          return page.goto(url).then(function () {
+            return page.content();
+          });
+        })
+        .then(function (html) {
+          // console.log(html);
+          data = extractor(html);
+          console.log("web scraping");
+          // console.log(data.text);
+          // target_text=data;
+          // compareText(data.text);
+          target_list.push(data.text);
 
-      if(url_list.length==target_list.length){
-        getTargetText();
-      }
-
-    })
-    .catch(function (err) {
-      // console.log("Scraping error - ", err);
+          if (url_list.length == target_list.length) {
+            getTargetText();
+          }
+        })
+        .catch(function (err) {
+          // console.log("Scraping error - ", err);
+        });
     });
-  });
-
-  
-
+  }
   
 
   function getTargetText() {
     target_list.forEach(function (target_text, index) {
-      console.log("compare text - ", index, " start");
+      // console.log("compare text - ", index, " start");
       var tokenize_target_document = tokenizer.tokenize(target_text);
       compareText(tokenize_target_document, index);
     });
@@ -219,27 +210,35 @@ router.route("/text").post(verifyToken,(req, res) => {
         tokenize_target_document
       );
 
-      if(target_index>0){
-        if(matches.bestMatch.rating>results_list[index][1]){
+      if (target_index > 0) {
+        if (matches.bestMatch.rating > results_list[index][1]) {
           // console.log(index,results_list[index][1],' ',matches.bestMatch.rating,);
-          results_list[index][1]=matches.bestMatch.rating;
-          results_list[index][2]=url_list[target_index];
+          results_list[index][1] = matches.bestMatch.rating;
+          results_list[index][2] = url_list[target_index];
         }
-      }else{
-        results_list.push([sentence, matches.bestMatch.rating, url_list[target_index]]);
+      } else {
+        results_list.push([
+          sentence,
+          matches.bestMatch.rating,
+          url_list[target_index],
+        ]);
       }
 
-      if(matches.bestMatch.rating>=0.8){
-        count+=1
+      if (matches.bestMatch.rating >= 0.8) {
+        count += 1;
       }
 
-      console.log("matches - ", index, " - ", results_list[index][1], " - ", results_list[index][2]);
+      console.log(
+        "matches - ",
+        index,
+        " - ",
+        results_list[index][1],
+        " - ",
+        results_list[index][2]
+      );
       // process+=1;
-      
     });
   }
-
-
 
   // tokenize_document.forEach(function (sentence, index) {
   //   var similarity = stringSimilarity.compareTwoStrings(sentence, traget_text);
@@ -278,27 +277,30 @@ router.route("/text").post(verifyToken,(req, res) => {
 // });
 
 router.route("/result").get(verifyToken, (req, res) => {
-  console.log("count-  ",count/tokenize_document.length*100);
-  if (results_list.length <=tokenize_document.length) {
+  console.log("count-  ", (count / tokenize_document.length) * 100);
+  if (results_list.length <= tokenize_document.length) {
     res.json({
       result: results_list,
-      plagiarism: count/tokenize_document.length*100,
+      plagiarism: (count / tokenize_document.length) * 100,
     });
   }
 });
 
 router.route("/result-save").post(verifyToken, (req, res) => {
   const files = results_list;
-  const checkType = 'web-plagiarism';
-  
-  const newResult = new Result_model({userId, files, checkType});
+  const checkType = "web-plagiarism";
 
-  newResult.save()
-    .then(() => res.json({
-        status: 'Result added!'
-      }))
-    .catch(function(error) {
-      res.status(400).json('Error: ' + error)       
+  const newResult = new Result_model({ userId, files, checkType });
+
+  newResult
+    .save()
+    .then(() =>
+      res.json({
+        status: "Result added!",
+      })
+    )
+    .catch(function (error) {
+      res.status(400).json("Error: " + error);
     });
 });
 
